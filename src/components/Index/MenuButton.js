1f5247dd437fe4e241d3f48537093f6de038bf7d
Seojunhwan <seojunhwan@kakao.com>
2023-04-16 20:16:48 +0900

    refactor: selectPage menubutton 통합

---
+++ src/components/Index/MenuButton.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { useHoverImage } from '../../hooks/useHoverImage';
+
+export const MenuButton = ({ defaultThumbnail, src: _src, onClick, ...restProps }) => {
+  const { src, onMouseOver, onMouseLeave } = useHoverImage(defaultThumbnail, _src);
+
+  return (
+    <Button onMouseOver={onMouseOver} onMouseLeave={onMouseLeave} onClick={onClick} {...restProps}>
+      <ButtonImg src={src} />
+    </Button>
+  );
+};
+
+const Button = styled.button`
+  cursor: pointer; // 커서 올리면 손바닥
+  // position : absolute;
+  background-color: transparent;
+  border: none;
+`;
+const ButtonImg = styled.img`
+  &:hover {
+    // 커서 올리면 이미지 커지게
+    // transform: scale(1.3);
+  }
+  width: 338px;
+  height: 80px;
+`;
