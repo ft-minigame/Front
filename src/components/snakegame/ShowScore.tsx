7f8d04dc4ad948856fc23bb0a3471d5f18fa5815
mingi1123 <min_gi1123@naver.com>
2023-05-13 18:48:38 +0900

    chore: remove unusing

--- src/components/snakegame/ShowScore.tsx
+++ src/components/snakegame/ShowScore.tsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import React from 'react';
-import { SnakeGameType } from '../../types/SnakeGameType';
 import { ShowScoreProps } from '../../types/ShowScoreProps'
 
 const CHARACTER_SIZE = 40;
