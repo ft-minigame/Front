376c4e88f36cec0469698016a6d5d34129a12991
mingi1123 <min_gi1123@naver.com>
2023-05-21 16:01:05 +0900

    feat: make gameoverModal

---
+++ src/components/modal/gameoverModal.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
+import gameoverImg from '../../assets/images/Modal/gameover/gameover.png';
+import restartImg from '../../assets/images/Modal/pause/restart.png';
+import menuImg from '../../assets/images/Modal/pause/menu.png';
+import { PauseModalType } from '../../types/PauseModalType';
+
+function GameoverModal({ restart }: PauseModalType ) {
+  const navigate = useNavigate();
+
+  const handleClick1 = () => {
+    navigate('/index');
+  };
+
+  const handleClick2 = () => {
+    restart();
+  };
+
+  return (
+    <Wrapper>
+      <PauseImgTag src={gameoverImg} />
+      <ButtonWrapper>
+        <Button onClick={handleClick2} >
+          <ButtonImg src={restartImg} />
+        </Button>
+        <Button onClick={handleClick1} >
+          <ButtonImg src={menuImg} />
+        </Button>
+      </ButtonWrapper>
+    </Wrapper>
+  );
+}
+
+export default GameoverModal;
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 350px;
+  height: 250px;
+  border: none;
+  padding: 20px;
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const PauseImgTag = styled.img`
+  position: absolute;
+  width: 350px;
+  height: 250px;
+  object-fit: cover;
+`
+const ButtonWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  top: 90px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  z-index: 1;
+  padding-right : 30px;
+  padding-left : 30px;
+`;
+
+const ButtonImg = styled.img`
+  width: 90px;
+  height: 36px;
+`;
--- src/components/snakegame/snakeGame.tsx
+++ src/components/snakegame/snakeGame.tsx
@@ -10,6 +10,7 @@ import itemImage from '../../assets/images/SnakeGame/item.png';
 import ShowScore from './ShowScore';
 import KeyPressHandle from './KeyPressHandle';
 import PauseModal from '../modal/pauseModal';
+import GameoverModal from '../modal/gameoverModal';
 import ReadyModal from '../modal/ReadyModal'
 import { SnakeGameType } from '../../types/SnakeGameType';
 import React from 'react';
@@ -30,6 +31,7 @@ const Game = () => {
   const [pause, setPause] = useState(false);
   const [item, setItem] = useState({ x: 0, y: 0, visible: false });
   const [score, setScore] = useState<number>(0);
+  const [showOverlay, setShowOverlay] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -81,12 +83,14 @@ const Game = () => {
     }
   
     if (head.x < 0 || head.x * CHARACTER_SIZE >= GAME_WIDTH || head.y < 0 || head.y * CHARACTER_SIZE >= GAME_HEIGHT) {
+      setShowOverlay(true);
       setGameOver(true);
       return;
     }
   
     const newSnake = [head, ...snake.slice(0, -1)];
     if (newSnake.length > 1 && newSnake.slice(1).some(block => block.x === head.x && block.y === head.y)) {
+      setShowOverlay(true);
       setGameOver(true);
       return;
     }
@@ -116,11 +120,13 @@ const Game = () => {
       setDirection(newDirection);
       setShowModal(true);
       setPause(true);
+      setShowOverlay(true);
     } else if (newDirection === 'close'){
       setDirection(tempDirec);
       setShowModal(false);
       setReady(true);
       setPause(false);
+      setShowOverlay(false);
     } else if (newDirection && !pause) {
       setDirection(newDirection);
       setTempDirec(newDirection);
@@ -135,12 +141,14 @@ const Game = () => {
     setPause(false);
     setItem({ x: 0, y: 0, visible: false });
     setScore(0);
+    setShowOverlay(false);
     setReady(true);
   };
 
   return (
     <Wrapper ref={inputRef} tabIndex={0} onKeyDown={handleKeyDown} >
       <Background />
+      {showOverlay && <Overlay />}
       <GameFrame />
       <Canvas>
         {snake.map((block, index) => (
@@ -149,7 +157,7 @@ const Game = () => {
         {item.visible && <SnakeBlock src={itemImage} style={{ left: item.x * CHARACTER_SIZE, top: item.y * CHARACTER_SIZE }} />}
         <ShowScore snake={snake} score={score} restProps={undefined} />
       </Canvas>
-      {gameOver && <GameOver>Game Over!</GameOver>}
+      {gameOver && <GameoverModal restart={restartGame} />}
       {showModal && <PauseModal restart={restartGame} />}
       {ready && <ReadyModal ready={ready} />}
     </Wrapper>
@@ -185,9 +193,12 @@ const SnakeBlock = styled.img`
   height: ${CHARACTER_SIZE}px;
 `;
 
-const GameOver = styled.div`
+const Overlay = styled.div`
   position: absolute;
-  font-size: 2rem;
-  color: red;
-  text-align: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3); /* Adjust the opacity as needed */
+  z-index: 998; /* Make sure the overlay appears on top of other elements */
 `;
